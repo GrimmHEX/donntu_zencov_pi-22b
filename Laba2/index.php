54e1166d2d894a2b95e31f50e0a6781460481965
Зенцов Данил (Grimm) <116502273+GrimmHEX@users.noreply.github.com>
2025-04-09 20:31:08 +0300

    Add files via upload

---
+++ Laba2/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/styles.css">
+    <title>Лабораторная работа 2</title>
+</head>
+<body>
+    <header>
+        <h1>Лабораторная работа 2</h1>
+        <nav>
+            <ul>
+                <li><a href="../index.html">Главная</a></li>
+                <li><a href="index.php">Лабораторная 2</a></li>
+                <li><a href="../Laba3/index.php">Лабораторная 3</a></li>
+                <li><a href="../Laba4/index.php">Лабораторная 4</a></li>
+				<li><a href="../Laba5/index.php">Лабораторная 5</a></li>
+                <li><a href="../Laba6/index.php">Лабораторная 6</a></li>
+                <li><a href="../Laba7/index.php">Лабораторная 7</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <h2>Результаты поиска слова "мель"</h2>
+<?php
+    include 'laba2.php'; // Включаем PHP файл
+    ?>
+	
+</main>
+</body>
+</html>
